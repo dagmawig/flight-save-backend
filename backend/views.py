c59b6c3f48253465a40e0069f1e319dbc940d23d
dagmawig <dgebreselasse@gmail.com>
2022-04-02 21:22:42 -0500

    connected django app to mongodb server, setup an example get method that returns JSON format data from an API

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,36 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+import requests
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+load_dotenv()
+
+connection_string = os.getenv('CONNECTION_STRING')
+
+client = MongoClient(connection_string)
+dbname = client['DATABASES']
+
+collection_name = dbname['collection1']
+
+priceline_api = {
+    "url": "https://priceline-com-provider.p.rapidapi.com/v1/flights/search",
+    "querystring": {"date_departure":"2022-06-17","class_type":"ECO","itinerary_type":"ONE_WAY","location_arrival":"NYC","location_departure":"STL","sort_order":"PRICE","duration_max":"2051","price_min":"100","number_of_passengers":"1","price_max":"20000","number_of_stops":"1"}, 
+    "headers": {
+	"X-RapidAPI-Host": "priceline-com-provider.p.rapidapi.com",
+	"X-RapidAPI-Key": "53ccb0e66bmsh883175eecd2c429p13df65jsna6c7198d36a1"
+}
+}
+
+@api_view(['GET'])
+def get(request):
+    if(request.method == "GET"):
+        print("gets here", request.body)
+        response = requests.request("GET", priceline_api['url'], headers=priceline_api['headers'], params=priceline_api['querystring'])
+        # response = requests.get("https://anapioficeandfire.com/api/houses/1")
+        print(response.text)
+        return Response(response.text)
--- flightsave/settings.py
+++ flightsave/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'backend',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,12 +75,12 @@ WSGI_APPLICATION = 'flightsave.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
